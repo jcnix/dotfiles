32270fb63d6ef0622c68f23a4ad674c54b507687
Casey Jones <jonescaseyb@johndeere.com>
2022-06-06 09:20:28 -0500

    more languages for treesitter

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-	ensure_installed = { "lua", "javascript", "typescript", "html", "css", "json", "scss", "yaml" },
+	ensure_installed = { "lua", "javascript", "typescript", "html", "css", "json", "scss", "yaml", "vim", "markdown", "bash" },
 	sync_install = false,
 	ignore_install = { "" },
 	highlight = {
