321844ba3389f48d5b1ef0ec4bceb355080143f6
Casey Jones <jonescaseyb@gmail.com>
2022-06-26 13:03:53 -0500

    update

--- work.zsh
+++ work.zsh
@@ -1,4 +1,8 @@
 export LIMA_INSTANCE=fedora
-export JAVA_HOME=`/usr/libexec/java_home`
+
+if [ -f /usr/libexec/java_home ]; then
+	export JAVA_HOME=`/usr/libexec/java_home`
+fi
+
 export PATH=/opt/homebrew/opt/python@3.9/libexec/bin:$PATH
 
