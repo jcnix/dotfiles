22ceb47015ad55c0d332a09bcdaca828dc69b952
Casey Jones <jonescaseyb@johndeere.com>
2022-07-26 11:50:23 -0500

    git blame settings

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -4,10 +4,10 @@ if not status_ok then
 end
 
 gitsigns.setup {
-	current_line_blame = true,
+	current_line_blame = false,
 	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = 'right_align',
+		virt_text_pos = 'eol',
 		delay = 0,
 		ignore_whitespace = false,
 	},
