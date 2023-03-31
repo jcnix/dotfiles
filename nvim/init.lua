515427998b9de623ddcbfb964fa4e14bc3ef2e34
Casey Jones <jonescaseyb@johndeere.com>
2023-03-31 13:18:05 -0500

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,29 +1,38 @@
-require "user.options"
--- vim.cmd[[colorscheme tokyonight]]
-vim.cmd "set guifont=JetBrainsMono\\ Nerd\\ Font:h13"
+if vim.g.vscode then
 
-require "user.colorscheme"
-require "user.keymaps"
-require "user.plugins"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.lualine"
-require "user.bufferline"
-require "user.nvimtree"
-require "user.gitsigns"
-require "user.whichkey"
-require "user.trouble"
-require "user.toggleterm"
-require "user.test"
-require "user.alpha"
-require "user.colorizer"
-require "user.gitlinker"
-require "user.neogit"
-require "user.ufo"
-require "user.blankline"
+	vim.opt.clipboard = 'unnamedplus'
+	require "vscode.nvim-ui"
 
--- vim.cmd "set completeopt=menu,menuone,noselect"
+else
+
+	require "user.options"
+	-- vim.cmd[[colorscheme tokyonight]]
+	vim.cmd "set guifont=JetBrainsMono\\ Nerd\\ Font:h13"
+
+	require "user.colorscheme"
+	require "user.keymaps"
+	require "user.plugins"
+	require "user.cmp"
+	require "user.lsp"
+	require "user.telescope"
+	require "user.treesitter"
+	require "user.autopairs"
+	require "user.lualine"
+	require "user.bufferline"
+	require "user.nvimtree"
+	require "user.gitsigns"
+	require "user.whichkey"
+	require "user.trouble"
+	require "user.toggleterm"
+	require "user.test"
+	require "user.alpha"
+	require "user.colorizer"
+	require "user.gitlinker"
+	require "user.neogit"
+	require "user.ufo"
+	require "user.blankline"
+
+	-- vim.cmd "set completeopt=menu,menuone,noselect"
+
+end
 
--- work.zsh
+++ work.zsh
@@ -1,4 +1,5 @@
 export LIMA_INSTANCE=fedora
+alias docker=nerdctl.lima
 
 if [ -f /usr/libexec/java_home ]; then
 	export JAVA_HOME=`/usr/libexec/java_home`
