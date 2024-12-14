66670b056ed411ba11ebfb4717a8e8ef49edbc20
Casey Jones <jonescaseyb@gmail.com>
2024-12-14 14:11:37 -0600

    use trouble for gR

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,4 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "References Quickfix" })
+-- vim.keymap.set("n", "gR", vim.lsp.buf.references, { desc = "References Quickfix" })
+vim.keymap.set(
+  "n",
+  "gR",
+  "<cmd>Trouble lsp_references toggle auto_refresh=false win.position=bottom<CR>",
+  { desc = "References (Trouble)" }
+)
