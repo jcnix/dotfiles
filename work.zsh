9d0fad8f3e0457e92c74904530342d6e9c2a1d15
Casey Jones <jonescaseyb@johndeere.com>
2024-12-14 12:03:05 -0600

    rootspec

--- work.zsh
+++ work.zsh
@@ -1,10 +1,10 @@
-export LIMA_INSTANCE=podman-vz
-alias docker=podman
-export DOCKER_HOST=unix:///Users/cj85430/.lima/podman-vz/sock/podman.sock               ─╯
+# export LIMA_INSTANCE=podman-vz
+# alias docker=podman
+# export DOCKER_HOST=unix:///Users/cj85430/.lima/podman-vz/sock/podman.sock               ─╯
 
 if [ -f /usr/libexec/java_home ]; then
 	export JAVA_HOME=`/usr/libexec/java_home`
 fi
 
-export PATH=/opt/homebrew/opt/python@3.9/libexec/bin:$PATH
+# export PATH=/opt/homebrew/opt/python@3.9/libexec/bin:$PATH
 
