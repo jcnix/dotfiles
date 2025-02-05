350b5ba1dbb405cc46c28ba0cf7fc8dae0c9cf75
Casey Jones <jonescaseyb@johndeere.com>
2025-02-05 14:10:32 -0600

    snacks picker

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  --@type snacks.Config
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+        },
+        grep = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
