c9c926063c07d91bcfa6f78a7a06f6143b6dbf78
Casey Jones <jonescaseyb@johndeere.com>
2025-04-02 10:41:42 -0500

    update gitbrowse patterns

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,5 +15,15 @@ return {
         },
       },
     },
+    gitbrowse = {
+      url_patterns = {
+        ["github%..*.com"] = {
+          branch = "/tree/{branch}",
+          file = "/blob/{branch}/{file}#L{line_start}-L{line_end}",
+          permalink = "/blob/{commit}/{file}#L{line_start}-L{line_end}",
+          commit = "/commit/{commit}",
+        },
+      },
+    },
   },
 }
